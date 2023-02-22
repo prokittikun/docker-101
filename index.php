9a700c5a3ce75cef4fa6d458dc7e776e7972710c
prokittikun <pro.kittikun.2546@gmail.com>
2023-02-23 02:24:34 +0700

    Init project php-docker-101:[docker-volume]

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Docker Compose</title>
+</head>
+
+<body>
+    <div style="max-height: 100vh;">
+        <div style="position: absolute; top: 0;bottom: 0;left: 0;right: 0;display:flex; justify-content: center; align-items: center; height: 100vh;">
+            <h1>Hi, This is my first <u><span style="color: red;">docker compose</span></u></h1>
+        </div>
+    </div>
+</body>
+
+</html>
